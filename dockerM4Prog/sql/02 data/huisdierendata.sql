824bbcebc02cdf4089cd781c0c371776ae193a64
KajLinthorst <37048@ma-web.nl>
2024-05-16 12:33:39 +0200

    02

---
+++ dockerM4Prog/sql/02 data/huisdierendata.sql
@@ -0,0 +1,8 @@
+INSERT INTO `huisdieren` (`id`, `naam`, `eigenaar`, `typedier`, `leeftijd`, `geboortedatum`) 
+VALUES 
+(NULL, 'Hendrik', 'Kaj', 'Hond', '7', '2017-03-14'),
+(NULL, 'Pietje', 'Saad', 'Kat', '4', '2020-01-11'),
+(NULL, 'Luigi', 'Mario', 'Vis', '22', '1995-08-17'),
+(NULL, 'Jan', 'Janno', 'Hond', '32', '1992-01-02'),
+(NULL, 'Cola', 'Akira', 'Rat', '1', '2023-16-5'),
+(NULL, 'Pietje', 'Saad', 'Kat', '5', '2020-02-04');
