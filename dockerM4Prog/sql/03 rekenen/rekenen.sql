aa4b3811972a519ecdde3211f3386092d59f4ceb
KajLinthorst <37048@ma-web.nl>
2024-05-24 09:51:52 +0200

    03 rekenen deel 1

---
+++ dockerM4Prog/sql/03 rekenen/rekenen.sql
@@ -0,0 +1,7 @@
+select sum(leeftijd) FROM `huisdieren`;
+
+SELECT AVG(leeftijd) AS gemiddelde_leeftijd FROM `huisdieren`;
+
+SELECT SUM(leeftijd) AS totaal_leeftijd, AVG(leeftijd) AS gemiddelde_leeftijd FROM huisdieren;
+
+SELECT AVG(leeftijd) AS gemiddelde_leeftijd FROM huisdieren;
